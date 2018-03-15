66ebc99132d1e288927c56f529e07eb31323f8b6
nzoghb <nzoghb@berkeley.edu>
2018-03-15 13:22:42 -0700

    comment added

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ from color_utils import *
 
 
 class Server(object):
-    # The following may be useful, `ps -fA | grep python`
     def __init__(self, host, port, protocol='tcp'):
-        self.clients = []
+        self.clients = {}
+        self.order = []
+        self.files = []
         self.online = False
         self.protocol = protocol
         self.sock = socket.socket()
@@ -30,20 +31,26 @@ class Server(object):
                 c, addr = self.sock.accept()
                 gprint("Got connection from", addr)
                 message = self.ret_msg(c)
-                response = self.process(message)
+                response = self.process(addr, message)
                 self.respond(c, response)
                 c.close()
-            # except ConnectionRefusedError:
-            #     rprint("Connection refused...")
             except KeyboardInterrupt:
+                # The following may be useful, `ps -fA | grep python`
                 rprint("\nKeyboardInterrupt...\nSocket closed.")
                 if c:
                     c.close()
                 break
 
-    def process(self, m):
+    def process(self, addr, m):
+        self.clients[addr] = m
+        self.order.append(addr)
         if m == 'PING':
             return 'PONG'
+        elif m.split(' ')[0] == 'QUERY':
+            if m.split(' ')[1] in self.files:
+                return 'QUERYHIT'
+            else:
+                return 'NOT FOUND'
         else:
             return 'hello, World!'
 
